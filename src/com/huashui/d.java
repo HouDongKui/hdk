bcaf094769119d20eae57e754e32d6b5677a3de5
Administrator <1374122150@qq.com>
2020-01-03 16:50:05 +0800

    first

---
+++ src/com/huashui/d.java
@@ -0,0 +1,7 @@
+package com.huashui;
+
+public class d {
+    public static void main(String[] args) {
+        System.out.println("5555555");
+    }
+}
---
+++ src/com/huashui/s.java
@@ -0,0 +1,7 @@
+package com.huashui;
+
+public class s {
+    public static void main(String[] args) {
+        System.out.println("999");
+    }
+}
